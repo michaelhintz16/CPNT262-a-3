282b6bfbc2e4e5272c83d667eda655ed9af49ae8
michaelhintz16 <michael.hintz@edu.sait.ca>
2021-03-12 00:22:15 -0700

    added files

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (request, response) => {
+  response.send('Hello World!')
+})
+
+app.use(function(request, response) {
+  response.status(404).sendFile(path.join(__dirname, 'public', '404.html'));  
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+  console.log(`Listening on port ${PORT}`);
+});
